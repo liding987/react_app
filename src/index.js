7cc90be12c1460d9a9993ab78e4fd4411c97221d
Li <Li@Lis-MacBook-Air.local>
2018-08-04 22:27:15 -0400

    fix issue for Mine

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const mine = require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+console.log(mine)
 
 render(
     <Router history={browserHistory} routes={routes} />,
