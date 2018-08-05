405be74e6840270579ba6bdea5aac1f8fe5df2b1
Li <Li@Lis-MacBook-Air.local>
2018-08-04 22:05:52 -0400

    fix issue for Mine

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 render(
     <Router history={browserHistory} routes={routes} />,
